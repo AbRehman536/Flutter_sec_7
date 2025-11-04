6c76175a7bc17817ba4321f1a5413da234fcbc6b
Abdullah <abdullahrehman1019@gmail.com>
2025-11-04 16:45:58 -0600

    Parameter done

--- lib/passing_parameters/screen_b.dart
+++ lib/passing_parameters/screen_b.dart
@@ -10,7 +10,7 @@ class ScreenB extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         title: Text("Screen B"),
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.blue,
         foregroundColor: Colors.black,
       ),
       body: Center(child: Column(children: [
